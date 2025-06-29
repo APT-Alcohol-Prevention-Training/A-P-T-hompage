cc3d35158626af46b31abd43772f7d2686721fdc
sueun-dev <sueun.dev@gmail.com>
2025-06-29 13:26:05 -0400

    update

--- app/onboarding/layout.js
+++ app/onboarding/layout.js
@@ -9,7 +9,6 @@ const OnboardingLayout = ({ children }) => {
   const { activeStep } = useOnboarding();
   return (
     <>
-     
       <div className="w-[90%] mx-auto max-w-2xl mt-[100px]">
         <ProgressBar />
         {activeStep && (
@@ -17,7 +16,6 @@ const OnboardingLayout = ({ children }) => {
             <h1 className="text-[#000000] text-[24px] font-medium">
               <Image src="/logo3.svg" width={150} height={150} alt="" />
             </h1>
-      
           </div>
         )}
         <div className="form-step">{children}</div>
